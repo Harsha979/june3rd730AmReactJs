06bc998216d34805fa7126fa9af0a0c1c3bef2b7
Harsha979 <dharshavardhanr@gmail.com>
2024-06-06 08:10:46 +0530

    added login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage(){
-    var userName="besant";
+    // var count=0;
+
+    // const [counter,setCounter] = useState(0);
+    const [userName,setUsername]=useState("");
+    const [password,setPassword]=useState("");
+
+    function checkLogin(){
+        if(userName !== ""){
+            if(password !== ""){
+                    alert("Login Success")
+            }else{
+                alert("password is required")
+            }
+        }else{
+            alert("UserName is required")
+        }
+    }
+
     return(
         <div style={{display:"grid",placeContent:"center"}}>
             <h1>LoginPage</h1>
             <label>UserName</label>
-            <input type="text" value={userName}></input>
+            <input type="text" value={userName} onChange={(e)=>{setUsername(e.target.value)}}></input>
             <label>Password</label>
-            <input type="text"></input>
+            <input type="password" value={password} 
+            onChange={(e)=>{setPassword(e.target.value)}}></input>
+            <button onClick={()=>{checkLogin()}}>LOGIN</button>
+            {/* <button onClick={()=>count=count+1}>updateCount</button>
+            count is : {count}
+            <button onClick={()=>setCounter((count)=>count+1)}>updateCounter</button>
+            counter is : {counter} */}
         </div>
     )
 }
